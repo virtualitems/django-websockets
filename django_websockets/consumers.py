e2bc562c56a6dfb2ce1d2e0c5ffa6e8517487d9f
Alejandro CR <contacto@alejandrocr.co>
2023-07-18 00:07:15 -0500

    group messages in SimpleConsumer

--- django_websockets/consumers.py
+++ django_websockets/consumers.py
@@ -14,12 +14,25 @@ class SimpleConsumer(AsyncWebsocketConsumer):
         """socket connection"""
         logging.debug('connected')
         await self.accept()
+        await self.channel_layer.group_add('simple', self.channel_name)
 
     async def disconnect(self, close_code):
         """socket disconnection"""
         logging.debug('disconnected: %s', close_code)
+        await self.channel_layer.group_discard('simple', self.channel_name)
 
     async def receive(self, text_data):
         """socket message"""
         logging.debug('received: %s', text_data)
-        await self.send(text_data=text_data)
+        await self.channel_layer.group_send(
+            'simple',
+            {
+                'type': 'simple.message',
+                'message': text_data,
+            }
+        )
+
+    async def simple_message(self, event):
+        """socket group message"""
+        logging.debug('group message: %s', event)
+        await self.send(text_data=event['message'])
