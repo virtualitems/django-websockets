a2f1d499ddaa4b361ce8844fce43a5363d90f3d7
Alejandro CR <contacto@alejandrocr.co>
2023-07-17 22:44:06 -0500

    added router for handle websockets to asgi.py

--- django_websockets/asgi.py
+++ django_websockets/asgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import URLRouter, ProtocolTypeRouter
+
+from django_websockets import routing
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_websockets.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': URLRouter(routing.websocket_urlpatterns),
+})
