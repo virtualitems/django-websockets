0a185cda946d18e04e4741391996cf6c31995e01
Alejandro CR <contacto@alejandrocr.co>
2023-07-17 23:00:34 -0500

    added templates folder to settings

--- django_websockets/settings.py
+++ django_websockets/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_websockets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
